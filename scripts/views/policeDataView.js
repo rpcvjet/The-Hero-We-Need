68a627e04744cd5063dfcb00bc0d2c909a9194a3
Ben Petty <benjamin.s.petty@gmail.com>
2016-11-15 15:06:01 -0800

    filter populates

--- scripts/views/policeDataView.js
+++ scripts/views/policeDataView.js
@@ -2,5 +2,16 @@
   'use strict';
   var policeDataView = {};
 
+  policeDataView.populateFilters = function() {
+    var options;
+    var template = Handlebars.compile($('#crimes-filter-template').text());
+    options = policeData.crimeFilter()
+    .map(function(crimes) {
+      console.log(crimes, 'CRIMES ****');
+      return template({val: crimes});
+    });
+    $('#crime-selector').append(options);
+  };
+
   module.policeDataView = policeDataView;
 }(window));
