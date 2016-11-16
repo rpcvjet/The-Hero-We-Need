5bc511357f23f3d78fa6f6e43b93cb386cfc1173
Ben Petty <benjamin.s.petty@gmail.com>
2016-11-16 14:06:40 -0800

    map scroll disabled and grayscale theme implemented

--- scripts/views/mapsDataView.js
+++ scripts/views/mapsDataView.js
@@ -13,7 +13,118 @@
       var map = new google.maps.Map(document.getElementById('map'), {
         center: new google.maps.LatLng(47.6, -122.3),
         zoom: 12,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        scrollwheel: false,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        // --------- Grayscale Map Styling ---------- //
+        styles: [{
+          'featureType': 'water',
+          'elementType': 'geometry',
+          'stylers': [{
+            'color': '#000000'
+          }, {
+            'lightness': 17
+          }]
+        }, {
+          'featureType': 'landscape',
+          'elementType': 'geometry',
+          'stylers': [{
+            'color': '#000000'
+          }, {
+            'lightness': 20
+          }]
+        }, {
+          'featureType': 'road.highway',
+          'elementType': 'geometry.fill',
+          'stylers': [{
+            'color': '#000000'
+          }, {
+            'lightness': 17
+          }]
+        }, {
+          'featureType': 'road.highway',
+          'elementType': 'geometry.stroke',
+          'stylers': [{
+            'color': '#000000'
+          }, {
+            'lightness': 29
+          }, {
+            'weight': 0.2
+          }]
+        }, {
+          'featureType': 'road.arterial',
+          'elementType': 'geometry',
+          'stylers': [{
+            'color': '#000000'
+          }, {
+            'lightness': 18
+          }]
+        }, {
+          'featureType': 'road.local',
+          'elementType': 'geometry',
+          'stylers': [{
+            'color': '#000000'
+          }, {
+            'lightness': 16
+          }]
+        }, {
+          'featureType': 'poi',
+          'elementType': 'geometry',
+          'stylers': [{
+            'color': '#000000'
+          }, {
+            'lightness': 21
+          }]
+        }, {
+          'elementType': 'labels.text.stroke',
+          'stylers': [{
+            'visibility': 'on'
+          }, {
+            'color': '#000000'
+          }, {
+            'lightness': 16
+          }]
+        }, {
+          'elementType': 'labels.text.fill',
+          'stylers': [{
+            'saturation': 36
+          }, {
+            'color': '#000000'
+          }, {
+            'lightness': 40
+          }]
+        }, {
+          'elementType': 'labels.icon',
+          'stylers': [{
+            'visibility': 'off'
+          }]
+        }, {
+          'featureType': 'transit',
+          'elementType': 'geometry',
+          'stylers': [{
+            'color': '#000000'
+          }, {
+            'lightness': 19
+          }]
+        }, {
+          'featureType': 'administrative',
+          'elementType': 'geometry.fill',
+          'stylers': [{
+            'color': '#000000'
+          }, {
+            'lightness': 20
+          }]
+        }, {
+          'featureType': 'administrative',
+          'elementType': 'geometry.stroke',
+          'stylers': [{
+            'color': '#000000'
+          }, {
+            'lightness': 17
+          }, {
+            'weight': 1.2
+          }]
+        }]
+        // ------- End Grayscale Map Styling ------------ //
       });
       for (var i = 0, length = policeDataArray.length; i < length; i++) {
         var data = policeDataArray[i];
