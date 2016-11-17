23f9eef0897da25a60bb552e0947ab1f6d3b66e1
Julien Wilson <julienawilson@gmail.com>
2016-11-17 11:49:51 -0800

    working on that routefix

--- scripts/controllers/mapsDataController.js
+++ scripts/controllers/mapsDataController.js
@@ -5,6 +5,7 @@
   mapsDataController.reveal = function() {
     $('.barny').fadeOut();
     $('#map').fadeIn();
+    $('#crimedata').fadeIn();
   };
 
   module.mapsDataController = mapsDataController;
